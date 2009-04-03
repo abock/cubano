4ab8200e6764df032ec7e12fbe70b3492fdd335b
Aaron Bockover <abockover@novell.com>
2009-04-03 15:41:37 -0400

    Subbed out the build and entry for the Cubano client program

---
+++ src/Cubano/Cubano.Client/Entry.cs
@@ -0,0 +1,17 @@
+using System;
+
+using Cairo;
+using Gtk;
+using Gtk.Moonlight;
+using System.Windows;
+
+namespace Cubano.Client
+{
+    public static class Entry
+    {
+        public static void Main ()
+        {
+        }
+    }
+}
+
