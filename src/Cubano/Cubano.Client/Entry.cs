9f985ed6a0aa05756c5fc2760451e322aeb5e9e4
Aaron Bockover <abockover@novell.com>
2009-04-03 20:34:45 -0400

    Updated to work against the new Moonlight.Gtk API

--- src/Cubano/Cubano.Client/Entry.cs
+++ src/Cubano/Cubano.Client/Entry.cs
@@ -2,8 +2,10 @@ using System;
 
 using Cairo;
 using Gtk;
-using Gtk.Moonlight;
+using Moonlight.Gtk;
 using System.Windows;
+
+using Application = Gtk.Application;
 
 namespace Cubano.Client
 {
@@ -11,6 +13,20 @@ namespace Cubano.Client
     {
         public static void Main ()
         {
+            Application.Init ();
+            XamlRuntime.Init ();
+
+            Window window = new Window ("Cubano");
+            window.SetDefaultSize (400, 300);
+            window.DeleteEvent += (o, e) => Application.Quit ();
+
+            XamlHost xaml_host = new XamlHost ();
+            xaml_host.LoadXaml ("<Canvas><Rectangle Width=\"50\" Height=\"50\" Fill=\"Green\"/></Canvas>");
+
+            window.Add (xaml_host);
+            window.ShowAll ();
+
+            Application.Run ();
         }
     }
 }
