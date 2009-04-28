6afd79bfae6e69a0b0bfcfdbbf65a1bc83522a2f
Aaron Bockover <abockover@novell.com>
2009-04-28 01:13:44 -0400

    Real work on the visualizer
    Logic for frame limiting and smoothing of frames. Reduces the resolution
    of the spectrum frames to optimize the rendering.
    
    Implemented goo and debug renderers.

--- src/Cubano/Cubano.Client/CubanoVisualizer.cs
+++ src/Cubano/Cubano.Client/CubanoVisualizer.cs
@@ -25,6 +25,7 @@
 // THE SOFTWARE.
 
 using System;
+using System.Collections.Generic;
 
 using Cairo;
 
@@ -35,10 +36,32 @@ namespace Cubano.Client
 {
     public class CubanoVisualizer : IDisposable
     {
+        private object render_points_mutext = new object ();
+        private Queue<float []> points = new Queue<float []> ();
+        
         public EventHandler<EventArgs> RenderRequest;
         
-        private float [][] last_pcm_data;
-        private float [][] last_spectrum_data;
+        private float [] render_points;
+        protected float [] RenderPoints {
+            get { return render_points; }
+        }
+        
+        private float render_loudness;
+        protected float RenderLoudness {
+            get { return render_loudness; }
+        }
+        
+        private int smoothing_passes = 5;
+        public int SmoothingPasses {
+            get { return smoothing_passes; }
+            set { smoothing_passes = value; }
+        }
+        
+        private int sample_points = 25;
+        public int SamplePoints {
+            get { return sample_points; }
+            set { sample_points = value; }
+        }
         
         private int height = 80;
         public int Height {
@@ -51,15 +74,35 @@ namespace Cubano.Client
             get { return width; }
             set { width = value; }
         }
+        
+        private Color color = new Color (0.2, 0.2, 0.2);
+        public Color Color {
+            get { return color; }
+            set { color = value; }
+        }
     
         public CubanoVisualizer ()
         {
             ConnectOrDisconnect (true);
+            RequestRender ();
         }
         
         public void Dispose ()
         {
             ConnectOrDisconnect (false);
+            RequestRender ();
+        }
+        
+        public void Connect ()
+        {
+            ConnectOrDisconnect (true);
+            RequestRender ();
+        }
+        
+        public void Disconnect ()
+        {
+            ConnectOrDisconnect (false);
+            RequestRender ();
         }
         
         private void ConnectOrDisconnect (bool connect)
@@ -84,28 +127,147 @@ namespace Cubano.Client
             }
         }
         
-        private void OnVisualizationDataAvailable (float [][] pcm, float [][] spectrum)
+        private void RequestRender ()
         {
-            last_pcm_data = pcm;
-            last_spectrum_data = spectrum;
             Banshee.Base.ThreadAssist.ProxyToMain (delegate {
                 OnRenderRequest ();
             });
         }
         
-        public void Render (Context cr)
+        private DateTime last_render = DateTime.MinValue;
+        private TimeSpan max_framerate = TimeSpan.FromSeconds (1.0 / 20.0);
+        
+        private void OnVisualizationDataAvailable (float [][] pcm, float [][] spectrum)
         {
-            if (last_pcm_data == null && last_spectrum_data == null) {
+            if (spectrum == null || DateTime.Now - last_render < max_framerate) {
                 return;
             }
 
-            cr.MoveTo (0, Height);
-            for (int i = 0; i < last_spectrum_data[0].Length; i+=2) {
-                cr.LineTo (i, Height - (Height * 1.5 * last_spectrum_data[0][i]));
+            lock (render_points_mutext) {
+                UpdatePoints (spectrum[0]);
             }
-            cr.Color = new Color (1, 0, 0, 0.5);
-            cr.LineTo (0, Height);
+            
+            RequestRender ();
+            
+            last_render = DateTime.Now;
+        }
+        
+        private float [] AnalyzeSpectrum (float [] spectrum, int sampleCount)
+        {   
+            int sample_idx = 0;
+            var samples = new float[sampleCount * 2];
+            
+            // Sample the spectrum to produce 2 * sampleCount points, creating
+            // a smoothed mirrored audio frame spectrum for averaging with
+            // previous AnalyzeSpectrum passes or suitable for direct rendering
+            for (int i = 0, n = spectrum.Length; i < n; i++) {
+                sample_idx = (int)Math.Round ((i / (double)n) * (sampleCount - 1));
+                int sample_idx_ofs = sampleCount - sample_idx - 1;
+                samples[sample_idx_ofs] += spectrum[i] / (n / (sampleCount - 1));
+                samples[sample_idx + sampleCount] = samples[sample_idx_ofs];
+            }
+            
+            // Find the maximum point in the computed points
+            float max = 0;
+            for (int i = 0; i < samples.Length; i++) {
+                max = Math.Max (max, samples[i]);
+            }
+            
+            // Fudge the mid point to be the max
+            samples[(int)Math.Floor (samples.Length / 2.0)] = max;
+            samples[(int)Math.Ceiling (samples.Length / 2.0)] = max;
+            
+            // Scale the set by the maximum point
+            for (int i = 0; i < samples.Length; i++) {
+                samples[i] /= (float)(1.0f - Math.Sqrt (max));
+            }
+            
+            return samples;
+        }
+        
+        private void UpdatePoints (float [] spectrum)
+        {
+            // Add the current analyzed audio frame and dequeue any expired frame
+            points.Enqueue (AnalyzeSpectrum (spectrum, SamplePoints));
+            if (points.Count > SmoothingPasses) {
+                points.Dequeue ();
+            }
+            
+            render_points = null;
+            render_loudness = 0;
+            
+            // Average all the queued frames into a frame suitable for direct rendering,
+            // creating very smooth visualization data; the higher SmoothingPasses is
+            // the smoother the data, but the more static (less motion)
+            foreach (var iter in points) {
+                if (render_points == null) {
+                    render_points = new float[iter.Length];
+                }
+                
+                for (int i = 0, n = Math.Min (iter.Length, render_points.Length); i < n; i++) {
+                    render_points[i] += iter[i] / (float)points.Count;
+                }
+            }
+            
+            for (int i = 0; i < render_points.Length / 2; i++) {
+                render_loudness = (float)Math.Max (render_loudness, render_points[i]);
+            }
+        }
+        
+        private void RenderDebug (Context cr)
+        {
+            double x = 320;
+            for (int i = 0, n = render_points.Length; i < n; i++) {
+                cr.Color = new Color (0, 1, 0);
+                cr.Rectangle (x, Height - Height * render_points[i], 10, Height * render_points[i]);
+                cr.Fill ();
+                cr.Color = new Color (0, 0, 0, 0.3);
+                cr.LineWidth = 1;
+                cr.Rectangle (x + 0.5, 0.5, 10 - 1, Height - 1);
+                cr.Stroke ();
+                x += 12;
+            }
+        }
+        
+        public void Render (Context cr)
+        {
+            lock (render_points_mutext) {
+                if (render_points == null) {
+                    return;
+                }
+                
+                RenderGoo (cr);
+                // RenderDebug (cr);
+            }
+        }
+        
+        private void RenderGoo (Context cr)
+        {
+            double max_r = Height / 2;
+            double x_ofs = Width / RenderPoints.Length;
+            double xc = 0, yc = Height;
+            
+            for (int i = 0, n = RenderPoints.Length; i < n; i++) {
+                xc += x_ofs;
+                cr.MoveTo (xc, yc);
+                cr.Arc (xc, yc, Height * RenderPoints[i], 0, 2 * Math.PI);
+            }
+            
+            cr.ClosePath ();
+            
+            var grad = new LinearGradient (0, 0, 0, Height);
+            
+            Color c = Color;
+            c.A = 0;
+            grad.AddColorStop (0, c);
+            
+            c.A = RenderLoudness / 2;
+            grad.AddColorStop (1, c);
+            
+            cr.Pattern = grad;
             cr.Fill ();
+            
+            grad.Destroy ();
         }
     }
 }
