0b5e0a4a0ba118363b3afc5f30a4fd44a10c7bf7
Aaron Bockover <abockover@novell.com>
2009-07-14 19:57:53 -0400

    Started working on new top-level source view

---
+++ src/Cubano/Banshee.Sources.Gui/CategorySourceView.cs
@@ -0,0 +1,129 @@
+// 
+// CategorySourceView.cs
+//  
+// Author:
+//   Aaron Bockover <abockover@novell.com>
+// 
+// Copyright 2009 Novell, Inc.
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+using System;
+using System.Collections.Generic;
+
+using Gtk;
+
+using Banshee.Sources;
+using Banshee.ServiceStack;
+using Banshee.Gui.Widgets;
+
+namespace Banshee.Sources.Gui
+{
+    public class CategorySourceView : HBox, IDisposable
+    {
+        private List<SourceLabel> labels = new List<SourceLabel> ();
+        
+        private class SourceLabel : InteractiveLabel
+        {
+            public SourceLabel ()
+            {
+                CurrentFontSizeEm = DefaultFontSizeEm = 1.5;
+                ActiveFontSizeEm = 1.6;
+            }
+        
+            private Source source;
+            public Source Source {
+                get { return source; }
+                set {
+                    source = value;
+                    Text = source.Name.ToLower ();
+                }
+            }
+        }
+    
+        public CategorySourceView ()
+        {
+            ServiceManager.SourceManager.SourceAdded += OnSourceUpdated;
+            ServiceManager.SourceManager.SourceRemoved += OnSourceUpdated;
+            ServiceManager.SourceManager.ActiveSourceChanged += OnActiveSourceChanged;
+            
+            Spacing = 15;
+        }
+        
+        public override void Dispose ()
+        {
+            ServiceManager.SourceManager.SourceAdded -= OnSourceUpdated;
+            ServiceManager.SourceManager.SourceRemoved -= OnSourceUpdated;
+            ServiceManager.SourceManager.ActiveSourceChanged -= OnActiveSourceChanged;
+            base.Dispose ();
+        }
+        
+        private void OnSourceUpdated (SourceEventArgs args)
+        {
+            Banshee.Base.ThreadAssist.ProxyToMain (delegate {
+                UpdateSources ();
+            });
+        }
+        
+        private void OnActiveSourceChanged (SourceEventArgs args)
+        {
+            Banshee.Base.ThreadAssist.ProxyToMain (delegate {
+                foreach (var label in labels) {
+                    label.IsActive = label.Source == args.Source;
+                }
+            });
+        }
+        
+        private bool ShouldShowSource (Source source)
+        {
+            return source != null && source.Parent == null &&
+                !source.UniqueId.StartsWith ("NowPlayingSource") &&
+                !source.UniqueId.StartsWith ("PlayQueueSource");
+        }
+
+        private void UpdateSources ()
+        {
+            int count = 0;
+        
+            foreach (var source in ServiceManager.SourceManager.Sources) {
+                if (!ShouldShowSource (source)) {
+                    continue;
+                }
+                
+                SourceLabel label = null;
+                if (count < labels.Count) {
+                    label = labels[count];
+                } else {
+                    labels.Add (label = new SourceLabel ());
+                    PackStart (label, false, false, 0);
+                    label.Show ();
+                }
+                
+                label.Source = source;
+                count++;
+            }
+            
+            for (int i = count; i < labels.Count; i++) {
+                Remove (labels[i]);
+            }
+            
+            labels.RemoveRange (count, labels.Count - count);
+        }
+    }
+}
