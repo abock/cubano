46699648e5c6d4ec9a857a8140ff59b52e934c6b
Aaron Bockover <abockover@novell.com>
2009-07-16 12:47:44 -0400

    Redesigned the Cubano header, it's hot now, but not hot enough

--- src/Cubano/Cubano.Client/CunanoHeader.cs
+++ src/Cubano/Cubano.Client/CunanoHeader.cs
@@ -40,51 +40,68 @@ namespace Cubano.Client
     {
         public SearchEntry SearchEntry { get; private set; }
         public Toolbar Toolbar { get; private set; }
+        public ActionLabel SourceBackButton { get; private set; }
     
         public CubanoHeader () : base (0.0f, 0.0f, 1.0f, 0.0f)
         {
             var action_service = ServiceManager.Get<InterfaceActionService> ();
             
+            var table = new Table (2, 3, false) {
+                ColumnSpacing = 20,
+                RowSpacing = 8
+            };
+            
+            table.Attach (new Alignment (0.0f, 0.0f, 0.0f, 0.0f) {
+                (SourceBackButton = new ActionLabel () {
+                    Text = "\u2190", 
+                    CurrentFontSizeEm = 2.0,
+                    DefaultFontSizeEm = 2.0,
+                    FontFamily = "DejaVu Sans"
+                })
+            }, 0, 1, 0, 1, AttachOptions.Shrink, AttachOptions.Fill | AttachOptions.Expand, 0, 0);
+            
+            table.Attach (new Alignment (1.0f, 0.0f, 0.0f, 0.0f) {
+                (SearchEntry = new SearchEntry ())
+            }, 1, 2, 0, 1, AttachOptions.Expand | AttachOptions.Fill, AttachOptions.Shrink, 0, 0);
+                
+            var action_box = new HBox ();
+            var align = new Alignment (0.0f, 0.0f, 0.0f, 0.0f) { action_box };
+            table.Attach (align, 2, 3, 0, 1, AttachOptions.Shrink, AttachOptions.Fill | AttachOptions.Expand, 0, 0);
+            
+            var import = new ActionLabel () { Text = Banshee.I18n.Catalog.GetString ("banshee-1", "Import") };
+            import.Activated += (o, e) => action_service.GlobalActions["ImportAction"].Activate ();
+            action_box.PackStart (import, false, false, 0);
+            
+            action_box.PackStart (new ActionLabel () { Text = "|", CanActivate = false }, false, false, 6);
+            
+            var preferences = new ActionLabel () { Text = Banshee.I18n.Catalog.GetString ("banshee-1", "Preferences") };
+            preferences.Activated += (o, e) => action_service.GlobalActions["PreferencesAction"].Activate ();
+            action_box.PackStart (preferences, false, false, 0);
+            
+            var close = new ActionLabel () { Text = "   \u2715", CurrentFontSizeEm = 1.2 };
+            close.Activated += (o, e) => action_service.GlobalActions["QuitAction"].Activate ();
+            action_box.PackStart (close, false, false, 0);
+            
+            // Second row
             Toolbar = (Toolbar)action_service.UIManager.GetWidget ("/HeaderToolbar");
             Toolbar.ShowArrow = false;
             Toolbar.ToolbarStyle = ToolbarStyle.BothHoriz;
             Toolbar.IconSize = IconSize.Menu;
             
-            // Capture the default children so we can hide them
-            // after we add Cubano-specific items to the toolbar
+            // Hide all default toolbar items, we only want the Banshee toolbar
+            // for sources that install action items there... ultimately sucky
             var children = Toolbar.Children;
-            
-            // Build the Cubano Toolbar
-            Toolbar.Insert (new GenericToolItem<Widget> (new RepeatActionButton ()), 0);
-            
-            var filler = new Alignment (0.5f, 0.5f, 0.75f, 0.0f);
-            filler.Add (SearchEntry = new SearchEntry ());
-            action_service.PopulateToolbarPlaceholder (Toolbar, "/HeaderToolbar/TrackInfoDisplay", filler, true);
-
-            var import = (ToolButton)action_service.GlobalActions["ImportAction"].CreateToolItem ();
-            import.IsImportant = true;
-            import.Label = Banshee.I18n.Catalog.GetString ("banshee-1", "Import");
-            Toolbar.Add (import);
-            
-            var prefs = (ToolButton)action_service.GlobalActions["PreferencesAction"].CreateToolItem ();
-            prefs.IsImportant = false;
-            Toolbar.Add (prefs);
-            
-            var quit = (ToolButton)action_service.GlobalActions["QuitAction"].CreateToolItem ();
-            quit.IsImportant = false;
-            quit.StockId = Stock.Close;
-            Toolbar.Add (quit);
-
-            // Show the Cubano items, hide the defaults we captured
-            Toolbar.ShowAll ();
             for (int i = 0; i < children.Length; i++) {
                 children[i].Visible = false;
             }
             
-            var vbox = new VBox ();
-            vbox.PackStart (Toolbar);
-            vbox.PackStart (new CategorySourceView ());
-            Add (vbox);
+            var box = new HBox ();
+            box.PackStart (new CategorySourceView (), false, false, 0);
+            box.PackEnd (Toolbar, false, false, 0);
+            table.Attach (box, 0, 3, 1, 2, 
+                AttachOptions.Expand | AttachOptions.Fill, AttachOptions.Shrink, 0, 0);
+            
+            Add (table);
             ShowAll ();
         }
     }
